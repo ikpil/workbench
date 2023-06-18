0d63b46a9b9fb689314e600a1bb27a36888fb898
ikpil <ikpil0@gmail.com>
2023-06-18 20:57:01 +0900

    흠

--- flutter-tutorials/my_app/lib/main.dart
+++ flutter-tutorials/my_app/lib/main.dart
@@ -11,17 +11,37 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          actions: [Icon(Icons.star), Icon(Icons.star)],
-          title: Text('dddd'),
-        ),
-        body: SizedBox(
-          child: IconButton(
-            icon: Icon(Icons.star),
-            onPressed: () {},
-          ),
-        ),
-      ),
+          appBar: AppBar(),
+          body: Container(
+            height: 150,
+            padding: EdgeInsets.all(10),
+            child: Row(
+              children: [
+                Image.asset(
+                  'camera.jpg',
+                  width: 150,
+                ),
+                Container(
+                  width: 200,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('카메라팝니다', style: TextStyle()),
+                      Text('금호동 3가'),
+                      Text('7000원'),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Icon(Icons.favorite),
+                          Text('4'),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
