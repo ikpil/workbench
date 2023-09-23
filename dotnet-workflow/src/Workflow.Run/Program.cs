d1ee1e8980fe7046826589cda12045b41fb05107
ikpil <ikpil0@gmail.com>
2023-09-23 12:01:32 +0900

    feature 2023.4.1

--- dotnet-workflow/src/Workflow.Run/Program.cs
+++ dotnet-workflow/src/Workflow.Run/Program.cs
@@ -13,3 +13,4 @@ Console.WriteLine($"2023.3.4-rc.1 {Calculator.Add(1, 2)}");
 Console.WriteLine($"2023.3.4-rc.2 {Calculator.Add(1, 2)}");
 Console.WriteLine($"2023.3.5 {Calculator.Add(1, 2)}");
 Console.WriteLine($"2023.3.7 {Calculator.Add(1, 2)}");
+Console.WriteLine($"2023.4.1 {Calculator.Add(1, 2)}");
