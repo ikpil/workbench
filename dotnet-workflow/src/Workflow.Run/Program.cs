27f20d3919f8abde599ce1382d87e023db911fb0
ikpil <ikpil0@gmail.com>
2023-09-23 11:30:34 +0900

    feature: test4

--- dotnet-workflow/src/Workflow.Run/Program.cs
+++ dotnet-workflow/src/Workflow.Run/Program.cs
@@ -7,3 +7,4 @@ Console.WriteLine($"Hello, World! - {Calculator.Add(1, 2)}");
 Console.WriteLine($"test1 - {Calculator.Add(1, 2)}");
 Console.WriteLine($"test2 - {Calculator.Add(1, 2)}");
 Console.WriteLine($"test3 - {Calculator.Add(1, 2)}");
+Console.WriteLine($"test4 - {Calculator.Add(1, 2)}");
