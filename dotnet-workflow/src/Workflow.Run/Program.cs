860fda3f640590fd2880f06467ca512f6b15c826
ikpil <ikpil0@gmail.com>
2023-09-23 12:02:02 +0900

    2022.12.1

--- dotnet-workflow/src/Workflow.Run/Program.cs
+++ dotnet-workflow/src/Workflow.Run/Program.cs
@@ -14,3 +14,4 @@ Console.WriteLine($"2023.3.4-rc.2 {Calculator.Add(1, 2)}");
 Console.WriteLine($"2023.3.5 {Calculator.Add(1, 2)}");
 Console.WriteLine($"2023.3.7 {Calculator.Add(1, 2)}");
 Console.WriteLine($"2023.4.1 {Calculator.Add(1, 2)}");
+Console.WriteLine($"2022.12.1 {Calculator.Add(1, 2)}");
