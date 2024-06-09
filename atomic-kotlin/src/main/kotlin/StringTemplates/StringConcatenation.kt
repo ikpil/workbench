a3554bcbf06d0d7c23e193586412e9e7ffe36846
ikpil <ikpil@naver.com>
2024-06-09 12:15:11 +0900

    atomic 8

---
+++ atomic-kotlin/src/main/kotlin/StringTemplates/StringConcatenation.kt
@@ -0,0 +1,10 @@
+package org.example.StringTemplates
+
+fun main() {
+    val s = "hi\n"
+    val n = 11000
+    val d = 3.1452
+
+    println("first: " + s + " second: " + n + " third: " + d)
+    println("ok")
+}
---
+++ atomic-kotlin/src/main/kotlin/StringTemplates/StringTemplates.kt
@@ -0,0 +1,7 @@
+package org.example.StringTemplates
+
+fun main() {
+    val answer = 42
+    println("Found $answer!")
+    println("printing a $1")
+}
