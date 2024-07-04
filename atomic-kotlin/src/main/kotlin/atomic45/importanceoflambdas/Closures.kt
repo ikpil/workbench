9ce2053a5b802acaa721a9c75e45c6caf42555c0
ikpil <ikpil@naver.com>
2024-07-05 00:41:47 +0900

    아토믹 코틀린 45. 람다의 중요성

---
+++ atomic-kotlin/src/main/kotlin/atomic45/importanceoflambdas/Closures.kt
@@ -0,0 +1,9 @@
+package org.example.atomic45.importanceoflambdas
+
+import atomictest.eq
+
+fun main() {
+    val list = listOf(1, 5, 7, 10)
+    val divider = 5
+    list.filter { it % divider == 0 } eq listOf(5, 10)
+}
