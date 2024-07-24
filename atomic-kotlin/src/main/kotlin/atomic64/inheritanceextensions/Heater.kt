b1155a2b6b9581538ec1cd63fe2e5eeb666c3380
ikpil <ikpil@naver.com>
2024-07-25 07:32:17 +0900

    아토믹 코틀린 64. 상속과 확장

---
+++ atomic-kotlin/src/main/kotlin/atomic64/inheritanceextensions/Heater.kt
@@ -0,0 +1,11 @@
+package org.example.atomic64.inheritanceextensions
+
+import atomictest.eq
+
+open class Heater {
+    fun heat(temperature: Int) = "heating to $temperature"
+}
+
+fun warm(heater: Heater) {
+    heater.heat(70) eq "heating to 70"
+}
