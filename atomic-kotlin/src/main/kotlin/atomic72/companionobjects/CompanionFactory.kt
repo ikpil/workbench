af30f6a32e897d6c281e1d3f3145b7d0813d0d3c
ikpil <ikpil@naver.com>
2024-10-05 01:21:18 +0900

    아토믹 코틀린 72. 동반 객체, 동반객체가 단 한번만 생성 된다는 사실을 알 수 있다

---
+++ atomic-kotlin/src/main/kotlin/atomic72/companionobjects/CompanionFactory.kt
@@ -0,0 +1,16 @@
+package org.example.atomic72.companionobjects
+
+import atomictest.eq
+
+class Numbered2
+private constructor(private val id: Int) {
+    override fun toString(): String = "#$id"
+    companion object Factory {
+        fun create(size: Int) = List(size) { Numbered2(it)}
+    }
+}
+
+fun main() {
+    Numbered2.create(0) eq "[]"
+    Numbered2.create(5) eq "[#0, #1, #2, #3, #4]"
+}
