ebdd71eec39f3b9cee81f01c634dbbd785d5c0db
ikpil <ikpil0@gmail.com>
2024-10-06 23:49:31 +0900

    아토믹 코틀린 73. 예외처리 (잘못 작성한 부분 수정)

--- atomic-kotlin/src/main/kotlin/atomic73/exceptionhandling/DefiningExceptions.kt
+++ atomic-kotlin/src/main/kotlin/atomic73/exceptionhandling/DefiningExceptions.kt
@@ -11,7 +11,7 @@ class Exception3(description: String): Exception2(description)
 fun main() {
     capture {
         throw Exception1(13)
-    } eq "Exception1: wrong value: 13"
+    } eq "Exception1: Wrong value: 13"
 
     capture {
         throw Exception3("error")
