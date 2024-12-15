f1343dc75f751c4b61c024d1fdd9828a76e3d47e
ikpil <ikpil@naver.com>
2024-12-15 13:42:28 +0900

    add maui

---
+++ MauiSandbox/MauiSandbox.Mac/AppDelegate.cs
@@ -0,0 +1,9 @@
+using Foundation;
+
+namespace MauiSandbox.Mac;
+
+[Register(nameof(AppDelegate))]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
---
+++ MauiSandbox/MauiSandbox.iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+using Foundation;
+
+namespace MauiSandbox.iOS;
+
+[Register(nameof(AppDelegate))]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
